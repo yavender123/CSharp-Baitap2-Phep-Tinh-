dc7812f35012522c7f44d2cd7a59b0a2ea3ca62a
Duc Anh <yavenderv@gmail.com>
2026-02-18 19:10:31 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Bai2
+{
+    static void Main(string[] args)
+    {
+        //Loi mo dau
+        Console.WriteLine($"Hello!");
+
+        //Dien so
+        Console.WriteLine($"Enter your number: ");
+        int number = int.Parse(Console.ReadLine()!);
+        
+        //Tinh phep tinh tong
+        Console.WriteLine($"Tong: {number} + {number} = {number+number}");
+
+        //Tinh phep tinh tru
+        Console.WriteLine($"Hieu: {number} - {number} = {number-number}");
+
+        //Tinh phep tinh nhan
+        Console.WriteLine($"Tich: {number} * {number} = {number*number}");
+
+        //Tinh phep tinh chia
+        Console.WriteLine($"Thuong: {number} / {number} = {number/number}");
+    }
+}
