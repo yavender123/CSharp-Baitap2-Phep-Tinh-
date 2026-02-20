f698f05a0846d06b897b2aa8bde3e423052acd5a
Duc Anh <yavenderv@gmail.com>
2026-02-20 09:29:33 +0700

    Cap nhat bai tap moi

--- Program.cs
+++ Program.cs
@@ -3,20 +3,20 @@ class Bai2
 {
     static void Main(string[] args)
     {
-        //Loi mo dau
+        //Loi khoi dau
         Console.WriteLine($"Hello!");
 
         //Nhap so
-        Console.WriteLine($"Number one: ");
+        Console.WriteLine($"Enter your number one: ");
         int number1 = int.Parse(Console.ReadLine()!);
 
-        Console.WriteLine($"Number two: ");
+        Console.WriteLine($"Enter your number two: ");
         int number2 = int.Parse(Console.ReadLine()!);
 
-        //Tinh
-        Console.WriteLine($"Tong: {number1} + {number2} = {number1 + number2}");
-        Console.WriteLine($"Hieu: {number1} - {number2} = {number1 - number2}");
-        Console.WriteLine($"Tich: {number1} * {number2} = {number1 * number2}");
-        Console.WriteLine($"Thuong: {number1} / {number2} = {number1 / number2}");
+        //Phep tinh va in ra destop
+        Console.WriteLine($"Tong: {number1 + number2}");
+        Console.WriteLine($"Hieu: {number1 - number2}");
+        Console.WriteLine($"Tich: {number1 * number2}");
+        Console.WriteLine($"Thuong: {number1 / number2}");
     }
 }
